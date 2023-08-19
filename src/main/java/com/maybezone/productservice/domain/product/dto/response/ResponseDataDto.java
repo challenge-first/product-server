9c15c886eccbafed59721953c130130942e0d532
kimdonghyun <ssk0080@gmail.com>
2023-08-20 02:09:12 +0900

    feat: global response entity

--- src/main/java/com/maybezone/productservice/domain/product/dto/response/ResponseDataDto.java
+++ src/main/java/com/maybezone/productservice/domain/product/dto/response/ResponseDataDto.java
@@ -1,4 +1,18 @@
 package com.maybezone.productservice.domain.product.dto.response;
 
-public class ResponseDataDto {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+public class ResponseDataDto<T> {
+
+    private T data;
+
 }
--- src/main/java/com/maybezone/productservice/domain/product/dto/response/ResponseMessageDto.java
+++ src/main/java/com/maybezone/productservice/domain/product/dto/response/ResponseMessageDto.java
@@ -1,4 +1,20 @@
 package com.maybezone.productservice.domain.product.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
 public class ResponseMessageDto {
+
+    private String message;
+    private Integer statusCode;
+    private String statusMessage;
+
 }
