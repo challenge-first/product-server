0f54d44e618acef255933994dc5917c964b5a4ac
kimdonghyun <ssk0080@gmail.com>
2023-08-20 02:46:37 +0900

    feat: #3 product dto

--- src/main/java/com/maybezone/productservice/domain/product/dto/request/RequestProductDto.java
+++ src/main/java/com/maybezone/productservice/domain/product/dto/request/RequestProductDto.java
@@ -1,4 +1,21 @@
 package com.maybezone.productservice.domain.product.dto.request;
 
+import com.maybezone.productservice.domain.product.productenum.MainCategory;
+import com.maybezone.productservice.domain.product.productenum.SubCategory;
+import lombok.*;
+
+import java.util.List;
+
+import static lombok.AccessLevel.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
 public class RequestProductDto {
+
+    private MainCategory mainCategory;
+    private List<SubCategory> subCategories;
+    private String searchWord;
+
 }
