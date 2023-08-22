5fe27a3cfc639b9aea7270093b4ad053bd9b89ba
kimdonghyun <ssk0080@gmail.com>
2023-08-22 17:03:13 +0900

    feat: #5 ProductRepository 생성

---
+++ src/main/java/com/maybezone/productservice/domain/product/repository/ProductQueryRepository.java
@@ -0,0 +1,66 @@
+package com.maybezone.productservice.domain.product.repository;
+
+import com.maybezone.productservice.domain.product.entity.Product;
+import com.maybezone.productservice.domain.product.entity.QProduct;
+import com.maybezone.productservice.domain.product.productenum.MainCategory;
+import com.maybezone.productservice.domain.product.productenum.SubCategory;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+import static com.maybezone.productservice.domain.product.entity.QProduct.*;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public List<Product> searchProducts(
+            List<MainCategory> mainCategories,
+            List<SubCategory> subCategories,
+            String searchWord,
+            Pageable pageable
+    ) {
+        return queryFactory
+                .selectFrom(product)
+                .where(
+                        equalMainCategory(mainCategories),
+                        equalSubCategory(subCategories),
+                        equalSearchWord(searchWord)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    private BooleanExpression equalMainCategory(List<MainCategory> mainCategories) {
+        if (mainCategories == null || mainCategories.isEmpty()) {
+            return null;
+        }
+
+        return product.mainCategory.in(mainCategories);
+    }
+
+    private BooleanExpression equalSubCategory(List<SubCategory> subCategories) {
+        if (subCategories == null || subCategories.isEmpty()) {
+            return null;
+        }
+
+        return product.subCategory.in(subCategories);
+    }
+
+    private BooleanExpression equalSearchWord(String searchWord) {
+        if (searchWord == null || searchWord.isEmpty()) {
+            return null;
+        }
+
+        return product.name.like("%" +searchWord + "%");
+    }
+
+}
--- src/main/java/com/maybezone/productservice/domain/product/repository/ProductRepository.java
+++ src/main/java/com/maybezone/productservice/domain/product/repository/ProductRepository.java
@@ -1,4 +1,17 @@
 package com.maybezone.productservice.domain.product.repository;
 
-public interface ProductRepository {
+import com.maybezone.productservice.domain.product.entity.Product;
+import com.maybezone.productservice.domain.product.productenum.MainCategory;
+import com.maybezone.productservice.domain.product.productenum.SubCategory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findTop4ByOrderByIdDesc(Pageable pageable);
+
 }
