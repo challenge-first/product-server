30385b7d2339e91d7bfaaf0542c6e7d04960f391
kimdonghyun <ssk0080@gmail.com>
2023-09-05 16:47:17 +0900

    refactor: #27 비관적 락 적용

--- src/main/java/com/maybezone/productservice/domain/product/repository/ProductRepository.java
+++ src/main/java/com/maybezone/productservice/domain/product/repository/ProductRepository.java
@@ -2,8 +2,15 @@ package com.maybezone.productservice.domain.product.repository;
 
 import com.maybezone.productservice.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
-import java.util.List;
+import java.util.Optional;
+
+import static jakarta.persistence.LockModeType.*;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Lock(PESSIMISTIC_READ)
+    Optional<Product> findById(Long id);
+
 }
